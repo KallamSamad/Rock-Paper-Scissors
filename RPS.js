78a6b0485fa829b48d5a399e9c7fda67fd95eafd
KallamSamad <kallamsamad@gmail.com>
2025-08-03 17:38:58 +0100

    Added the getHumanChoice function and made sure that it repeats the question until you get RPS

--- RPS.js
+++ RPS.js
@@ -1,20 +1,44 @@
  console.log("The JS file is connected")
 
+ state =false
+
  function getComputerChoice (){
 
-    let choice=Math.floor(Math.random()*3)+1
-    if (choice===1){
+    let compChoice=Math.floor(Math.random()*3)+1
+    if (compChoice===1){
         return console.log("rock")
     }
     
-    else if (choice===2){
+    else if (compChoice===2){
         return console.log("paper")
     }
 
-    else if (choice===3){
+    else if (compChoice===3){
         return console.log("scissors")
     }
  }
 
 
  getComputerChoice()
+
+ function getHumanChoice(){
+    askUser=prompt("Rock, paper or Scissors?").trim().replace(/[A-Z]/g, '')
+    if (askUser == "rock") (
+        console.log("You chose rock")
+    )
+    else if  (askUser == "paper"){
+        console.log(" You chose paper")
+    }
+        
+    else if (askUser == "scissors"){
+        console.log("You chose scissors")
+    }
+    
+    else{
+        getHumanChoice()
+    }
+        
+}
+    
+
+ getHumanChoice()
