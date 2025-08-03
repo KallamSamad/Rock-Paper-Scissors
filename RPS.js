cf922d50f868395cd2b8a3640fa5c2208296f89a
KallamSamad <kallamsamad@gmail.com>
2025-08-03 18:39:56 +0100

    Completed quiz logic by ading the playround function and using humnachoice and comp chocie as parameters

--- RPS.js
+++ RPS.js
@@ -1,45 +1,82 @@
- console.log("The JS file is connected")
+let humanScore =0
+let compScore=0
 
- let score =0
- state =false
 
  function getComputerChoice (){
 
     let compChoice=Math.floor(Math.random()*3)+1
     if (compChoice===1){
-        return console.log("rock")
+        console.log("Computer chose rock")
+        return "rock"
     }
     
     else if (compChoice===2){
-        return console.log("paper")
+         console.log("Computer chose paper")
+        return "paper"
     }
 
     else if (compChoice===3){
-        return console.log("scissors")
+        console.log("Computer chose scissors")
+        return "scissors"
     }
  }
 
 
- getComputerChoice()
 
  function getHumanChoice(){
     askUser=prompt("Rock, paper or Scissors?").trim().replace(/[A-Z]/g, '')
-    if (askUser == "rock") (
+    if (askUser == "rock") {
         console.log("You chose rock")
-    )
+        return  "rock" 
+    }
     else if  (askUser == "paper"){
         console.log(" You chose paper")
+        return "paper"
     }
         
     else if (askUser == "scissors"){
         console.log("You chose scissors")
+        return "scissors"
     }
     
     else{
-        getHumanChoice()
+       return getHumanChoice()
+        
     }
         
 }
-    
 
- getHumanChoice()
+function playRound(humanChoice, computerChoice){
+    if ( humanChoice == "paper" && computerChoice =="rock"){
+        humanScore++
+        console.log(`You Win! Your score is ${humanScore} and the computers score is ${compScore}` )
+    }
+    else if ( humanChoice == "rock" && computerChoice =="paper"){
+        compScore++
+        console.log(`You Lose! Your score is ${humanScore} and the computers score is ${compScore}` )
+    }
+    else if ( humanChoice == "paper" && computerChoice =="scissors"){
+         compScore++
+          console.log(`You Lose! Your score is ${humanScore} and the computers score is ${compScore}` )
+    }
+    else if ( humanChoice == "scissors" && computerChoice =="paper"){
+        humanScore++
+        console.log(`You win! Your score is ${humanScore} and the computers score is ${compScore}` )
+    }
+    else if ( humanChoice == "scissors" && computerChoice =="rock"){
+        compScore++
+         console.log(`You Lose! Your score is ${humanScore} and the computers score is ${compScore}` )
+    }
+    else if ( humanChoice== "rock" && computerChoice =="scissors"){
+        humanScore++
+        console.log(`You win! Your score is ${humanScore} and the computers score is ${compScore}` )
+
+    }
+     else if ( humanChoice== computerChoice) {
+        console.log("Tie, try again")
+        console.log(` Your score is ${humanScore} and the computers score is ${compScore}` )
+    }
+playRound(getComputerChoice(),getHumanChoice())
+}
+
+playRound(getComputerChoice(),getHumanChoice())
