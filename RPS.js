d20995af97bbb48de3a487f8edc77b244698b317
KallamSamad <kallamsamad@gmail.com>
2025-08-07 15:03:43 +0100

    Added eventlsitenres to change input to button press

--- RPS.js
+++ RPS.js
@@ -23,8 +23,7 @@ let compScore=0
 
 
 
- function getHumanChoice(){
-    askUser=prompt("Rock, paper or Scissors?").trim().replace(/[A-Z]/g, '')
+ function getHumanChoice(askUser){
     if (askUser == "rock") {
         console.log("You chose rock")
         return  "rock" 
@@ -49,32 +48,40 @@ let compScore=0
 function playRound(humanChoice, computerChoice){
     if ( humanChoice == "paper" && computerChoice =="rock"){
         humanScore++
-        console.log(`You Win! Your score is ${humanScore} and the computers score is ${compScore}` )
+        humanCount.textContent=`Your score is ${humanScore} `
+        compCount.textContent=`Computer score is ${compScore}` 
     }
     else if ( humanChoice == "rock" && computerChoice =="paper"){
         compScore++
-        console.log(`You Lose! Your score is ${humanScore} and the computers score is ${compScore}` )
+        humanCount.textContent =`Your score is ${humanScore}` 
+        compCount.textContent=`Computer score is ${compScore}` 
     }
     else if ( humanChoice == "paper" && computerChoice =="scissors"){
          compScore++
-          console.log(`You Lose! Your score is ${humanScore} and the computers score is ${compScore}` )
+          humanCount.textContent=`Your score is ${humanScore}}`
+           compCount.textContent=`Computer score is ${compScore}` 
     }
     else if ( humanChoice == "scissors" && computerChoice =="paper"){
         humanScore++
-        console.log(`You win! Your score is ${humanScore} and the computers score is ${compScore}` )
+        humanCount.textContent =` Your score is ${humanScore}`
+         compCount.textContent=`Computer score is ${compScore}`  
     }
     else if ( humanChoice == "scissors" && computerChoice =="rock"){
         compScore++
-         console.log(`You Lose! Your score is ${humanScore} and the computers score is ${compScore}` )
+         humanCount.textContent=`Your score is ${humanScore} `
+         compCount.textContent=`Computer score is ${compScore}` 
+         
     }
     else if ( humanChoice== "rock" && computerChoice =="scissors"){
         humanScore++
-        console.log(`You win! Your score is ${humanScore} and the computers score is ${compScore}` )
+        humanCount.textContent =`Your score is ${humanScore} ` 
+         ompCount.textContent=`Computer score is ${compScore}` 
 
     }
      else if ( humanChoice== computerChoice) {
-        console.log("Tie, try again")
-        console.log(` Your score is ${humanScore} and the computers score is ${compScore}` )
+       humanCount.textContent="Tie, try again"
+       humanCount.textContent=` Your score is ${humanScore} ` 
+       compCount.textContent=`Computer score is ${compScore}` 
     }
 playRound(getComputerChoice(),getHumanChoice())
 }
@@ -83,30 +90,46 @@ const division =document.createElement("div");
 division.style.backgroundColor = "pink";
 document.body.appendChild(division);
 
- 
+
+const humanCount = document.createElement("div")
+humanCount.innerHTML = ""
+humanCount.style.color="green"
+humanCount.classList.add("humanCount")
+division.appendChild(humanCount)
+
+const compCount= document.createElement("div")
+compCount.innerHTML=""
+compCount.style.color="red"
+compCount.classList.add("compCount")
+division.appendChild(compCount)
 
 const container = document.createElement("div")
 container.classList.add("container")
 division.appendChild(container)
 
+
 const compCont = document.createElement("div")
 compCont.style.backgroundColor ="gold"
+compCont.classList.add("compCont")
 division.appendChild(compCont)
 
 const playBtn1 = document.createElement("button");
 playBtn1.textContent = "Rock";
 playBtn1.style.backgroundColor="red";
 container.appendChild(playBtn1);
+playBtn1.addEventListener("click", ()=> getComputerChoice("rock"))
 
 const playBtn2 = document.createElement("button");
 playBtn2.textContent = "Paper";
 playBtn2.style.backgroundColor="blue";
 container.appendChild(playBtn2);
+playBtn2.addEventListener("click", ()=> getComputerChoice("paper"))
 
 const playBtn3 = document.createElement("button");
 playBtn3.textContent = "Scissors";
 playBtn3.style.backgroundColor="green";
 container.appendChild(playBtn3);
+playBtn3.addEventListener("click", ()=> getComputerChoice("scissors"))
 
 const computerOutput = document.createElement("div");
 computerOutput.innerHTML = ""
